df9e50754a3d81607b160dcd05b46fe43a8682b6
sdhawan8 <43023063+shaand1@users.noreply.github.com>
2021-10-14 22:00:30 -0400

    app asks to turn on bluetooth if disabled

---
+++ app/src/main/java/com/example/vescdatalogger/LocationPermissionFragment.java
@@ -0,0 +1,29 @@
+package com.example.vescdatalogger;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class LocationPermissionFragment extends DialogFragment {
+
+    private final int LOCATION_PERMISSION_REQUEST_CODE = 2;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Location permission required");
+        builder.setMessage("System requires apps to be granted location access in order to scan for BLE devices.");
+        builder.setCancelable(false);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/vescdatalogger/MainActivity.java
+++ app/src/main/java/com/example/vescdatalogger/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.vescdatalogger;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.vescdatalogger.ui.main.DataFragment;
@@ -9,19 +16,57 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.vescdatalogger.ui.main.SectionsPagerAdapter;
 import com.example.vescdatalogger.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private final int ENABLE_BLUETOOTH_REQUEST_CODE = 1;
+
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    private void startBLEscan() {
+        if (!hasLocationPermission()) {
+
+        } else {
+
+        }
+    }
+
+    private void requestLocationPermission() {
+        LocationPermissionFragment locationAlert = new LocationPermissionFragment();
+        //locationAlert.show(getSupportFragmentManager(), "locationAlertDialog");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!bluetoothAdapter.isEnabled()) {
+            promptEnableBluetooth();
+        }
+    }
+
+    private void promptEnableBluetooth() {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE);
+    }
+
+    private boolean hasLocationPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +92,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button scanButton = findViewById(R.id.button2);
+        /*scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startBLEscan();
+            }
+        });*/
     }
 }

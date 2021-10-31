463482d0de0bd91e8625537680c82462cbeabc53
sdhawan8 <43023063+shaand1@users.noreply.github.com>
2021-10-31 18:51:33 -0400

    uses graphview library to plot line graph

--- app/src/main/java/com/example/vescdatalogger/ui/main/DataFragment.java
+++ app/src/main/java/com/example/vescdatalogger/ui/main/DataFragment.java
@@ -1,6 +1,7 @@
 package com.example.vescdatalogger.ui.main;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,9 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.vescdatalogger.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class DataFragment extends Fragment {
     private static final String TAG = "DataFragment";
@@ -16,6 +20,15 @@ public class DataFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_data, container, false);
 
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
         return view;
     }
 

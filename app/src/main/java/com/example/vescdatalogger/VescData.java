e321a7ebc6b75bba745cfea940df258f412f897c
sdhawan8 <43023063+shaand1@users.noreply.github.com>
2021-11-27 23:21:11 -0500

    file fragment layout

--- app/src/main/java/com/example/vescdatalogger/VescData.java
+++ app/src/main/java/com/example/vescdatalogger/VescData.java
@@ -15,12 +15,41 @@ public class VescData {
     private VescData(){
         //test = 1;
     }
+
+    float minBatteryCurrent;
+    float maxBatteryCurrent;
+    float avgBatteryCurrent;
+    float minRPM;
+    float maxRPM;
+    float avgRPM;
+    float minMosfetTemp;
+    float maxMosfetTemp;
+    float avgMosfetTemp;
+
     /*public int getTest() {
         return test;
     }*/
     public void addMessage(Message newMessage) {
+        int oldSize = messageQueue.size();
         messageQueue.add(newMessage);
+        int newSize = messageQueue.size();
         Log.i("messagequeue", "added a message");
+        float avgCalc = 0;
+        float newBatteryCurrent = newMessage.getParameter("Battery Current");
+        if (newBatteryCurrent < minBatteryCurrent) minBatteryCurrent = newBatteryCurrent;
+        else if (newBatteryCurrent > maxBatteryCurrent) maxBatteryCurrent = newBatteryCurrent;
+        avgCalc = ((avgBatteryCurrent * oldSize) + newBatteryCurrent) / newSize;
+        avgBatteryCurrent = avgCalc;
+        float newRPM = newMessage.getParameter("ERPM");
+        if (newRPM < minRPM) minRPM = newRPM;
+        else if (newBatteryCurrent > maxRPM) maxRPM = newRPM;
+        avgCalc = ((avgRPM * oldSize) + newRPM) / newSize;
+        avgRPM = avgCalc;
+        float newMosfetTemp = newMessage.getParameter("MOSFET Temp");
+        avgCalc = ((avgMosfetTemp * oldSize) + newMosfetTemp) / newSize;
+        if (newMosfetTemp < minMosfetTemp) minMosfetTemp = newMosfetTemp;
+        else if (newMosfetTemp > maxMosfetTemp) maxMosfetTemp = newMosfetTemp;
+        avgMosfetTemp = avgCalc;
     }
 
     public Message getRecent() {
